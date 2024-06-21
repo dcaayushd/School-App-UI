72d5ea8c7b68f24ddd0683eee9606315db72b3f6
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-21 23:44:49 +0545

    UI Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:scholae/constants.dart';
 import 'package:scholae/routes.dart';
-import 'package:scholae/screens/splash_screen.dart';
+import 'package:scholae/theme.dart';
+import 'package:sizer/sizer.dart';
+
+import 'screens/splash_screen/splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,69 +17,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Scholae',
-      // We're using light theme for the app
-      theme: ThemeData.light().copyWith(
-        // Scaffold Default Color
-        scaffoldBackgroundColor: kPrimaryColor,
-        primaryColor: kPrimaryColor,
-        // Use google fonts fpr our app, we will use sourceSansPro/sourceSans3
-        textTheme: GoogleFonts.sourceSans3TextTheme(Theme.of(context).textTheme)
-            .apply()
-            .copyWith(
-              bodyLarge: TextStyle(
-                color: kTextWhiteColor,
-                fontSize: 35.0,
-                fontWeight: FontWeight.bold,
-              ),
-              labelMedium: TextStyle(
-                color: kTextWhiteColor,
-                fontSize: 18.0,
-                fontWeight: FontWeight.w300,
-              ),
-            ),
-        // Input Decoration for all our app
-        inputDecorationTheme: InputDecorationTheme(
-          labelStyle: TextStyle(
-            fontSize: 15.0,
-            color: kTextLightColor,
-            height: 0.5,
-          ),
-          hintStyle: TextStyle(
-            fontSize: 16.0,
-            color: kTextBlackColor,
-            height: 0.5,
-          ),
-          // We are using Underline Input Border not Outline
-          enabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: kTextLightColor,
-              width: 0.7,
-            ),
-          ),
-          border: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: kTextLightColor,
-            ),
-          ),
-          disabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: kTextLightColor,
-            ),
-          ),
-          focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: kPrimaryColor,
-            ),
-          ),
-        ),
-      ),
-      // Initial Route is Splash Screen which means first screen
-      initialRoute: SplashScreen.routeName,
-      // Define the routes file in order to access the routes any where all over the app.
-      routes: routes,
-    );
+    //it requires 3 parameters
+    //context, orientation, device
+    //it always requires, see plugin documentation
+    return Sizer(builder: (context, orientation, device){
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Scholae Brain',
+        theme: CustomTheme().baseTheme,
+        //initial route is splash screen
+        //mean first screen
+        initialRoute: SplashScreen.routeName,
+        //define the routes file here in order to access the routes any where all over the app
+        routes: routes,
+      );
+    });
   }
 }
