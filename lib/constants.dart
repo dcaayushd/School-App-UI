733ced916ca22fe8ea2e973730a788f814f6b077
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-22 14:24:36 +0545

    Updated

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
 
-//Colors
+//colors
 const Color kPrimaryColor = Color(0xFF345FB4);
 const Color kSecondaryColor = Color(0xFF6789CA);
 const Color kTextBlackColor = Color(0xFF313131);
@@ -10,7 +12,7 @@ const Color kOtherColor = Color(0xFFF4F6F7);
 const Color kTextLightColor = Color(0xFFA5A5A5);
 const Color kErrorBorderColor = Color(0xFFE74C3C);
 
-//Default value
+//default value
 const kDefaultPadding = 20.0;
 
 const sizedBox = SizedBox(
@@ -28,9 +30,27 @@ const kHalfWidthSizedBox = SizedBox(
   width: kDefaultPadding / 2,
 );
 
-//Validation for mobile
+final kTopBorderRadius = BorderRadius.only(
+  topLeft: Radius.circular(SizerUtil.deviceType == DeviceType.tablet ? 40 : 20),
+  topRight:
+      Radius.circular(SizerUtil.deviceType == DeviceType.tablet ? 40 : 20),
+);
+
+final kBottomBorderRadius = BorderRadius.only(
+  bottomRight: Radius.circular(SizerUtil.deviceType == DeviceType.tablet ? 40 : 20),
+  bottomLeft:
+  Radius.circular(SizerUtil.deviceType == DeviceType.tablet ? 40 : 20),
+);
+
+final kInputTextStyle = GoogleFonts.poppins(
+  color: kTextBlackColor,
+  fontSize: 11.sp,
+  fontWeight: FontWeight.w500
+);
+
+//validation for mobile
 const String mobilePattern = r'(^(?:[+0]9)?[0-9]{10,12}$)';
 
-//Validation for email
+//validation for email
 const String emailPattern =
     r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,20 @@
-import 'package:flutter/material.dart';
-import 'package:scholae/screens/login_screen.dart';
-import 'package:scholae/screens/splash_screen.dart';
+
+import 'package:flutter/cupertino.dart';
+import 'package:scholae/screens/splash_screen/splash_screen.dart';
+import 'screens/assignment_screen/assignment_screen.dart';
+import 'screens/datesheet_screen/datesheet_screen.dart';
+import 'screens/fee_screen/fee_screen.dart';
+import 'screens/home_screen/home_screen.dart';
+import 'screens/login_screen/login_screen.dart';
+import 'screens/my_profile/my_profile.dart';
 
 Map<String, WidgetBuilder> routes = {
-  // All Screens will be registered here like manifest in Android.
-  SplashScreen.routeName : (context) => SplashScreen(),
-  LoginScreen.routeName : (context) => LoginScreen(),
+  //all screens will be registered here like manifest in android
+  SplashScreen.routeName: (context) => SplashScreen(),
+  LoginScreen.routeName: (context) => LoginScreen(),
+  HomeScreen.routeName: (context) => HomeScreen(),
+  MyProfileScreen.routeName: (context) => MyProfileScreen(),
+  FeeScreen.routeName: (context) => FeeScreen(),
+  AssignmentScreen.routeName: (context) => AssignmentScreen(),
+  DateSheetScreen.routeName: (context) => DateSheetScreen(),
 };
