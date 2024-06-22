d522cabdee27e3d9837c4b74a94fcbcd2d33f3ca
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-22 14:22:18 +0545

    Custom Buttons Added

---
+++ lib/components/custom_buttons.dart
@@ -0,0 +1,48 @@
+import 'package:scholae/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class DefaultButton extends StatelessWidget {
+  final VoidCallback onPress;
+  final String title;
+  final IconData iconData;
+
+  const DefaultButton(
+      {super.key,
+      required this.onPress,
+      required this.title,
+      required this.iconData});
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPress,
+      child: Container(
+        padding: EdgeInsets.only(right: kDefaultPadding),
+        width: 100.w,
+        height: SizerUtil.deviceType == DeviceType.tablet ? 9.h : 7.h,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [kSecondaryColor, kPrimaryColor],
+              begin: const FractionalOffset(0.0, 0.0),
+              end: const FractionalOffset(0.5, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+            borderRadius: BorderRadius.circular(kDefaultPadding)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            Text(title, style: Theme.of(context).textTheme.titleSmall),
+            Spacer(),
+            Icon(
+              iconData,
+              size: 26.sp,
+              color: kOtherColor,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
